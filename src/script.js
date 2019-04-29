f350353af9a8329a24e59f9cba6eab36de381d13
Yurdanov Sergey <yurdik1987@gmail.com>
2019-04-29 11:04:26 +0300

    add js tren datas

--- src/script.js
+++ src/script.js
@@ -1,5 +1,38 @@
-function hello() {
-    prompt('how are you?', 'i am fine');
-    let mood = 'welcome to our site';
-    document.write(mood);
-}
+// function hello() {
+//     prompt('how are you?', 'i am fine');
+//     let mood = 'welcome to our site';
+//     document.write(mood);
+// }
+// ;(function () {
+//     let a = 0;
+//     while ( a < 3 ) { /* пока а < 3 будет идти проверка */
+//     alert( a );
+//     a++; 
+// }
+// }) ();
+
+// ;(function () {
+//     let a = 0;
+//     do { /* пока а < 3 будет идти проверка */
+//     alert( a );
+//     a++; 
+// }
+// while ( a < 3 );
+// }) ();
+
+// let sum = 0;
+// let value = 0;
+// while (true) {
+//     if (!value) break;
+//     sum += value;
+// }
+// alert(sum);
+
+for (var i = 0; i < 20; i++) {
+
+    if (i % 2 == 0) continue;
+  
+    alert(i);
+  }
+
+
